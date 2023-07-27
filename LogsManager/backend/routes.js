75fbb9a9e5c7064b13b3ee9d8058bfb3f2f1732d
Viniz <vini.zig@hotmail.com>
2023-07-27 14:04:44 -0300

    Commit Routes Correction

--- LogsManager/backend/routes.js
+++ LogsManager/backend/routes.js
@@ -166,7 +166,6 @@ router.get('/logs', (req, res) => {
 });
 
 
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'uploads');
@@ -179,10 +178,6 @@ const storage = multer.diskStorage({
   },
 });
 
-
-
-
-const upload = multer({ storage });
 
 // Upload process
 router.post('/upload', upload.single('file'), (req, res) => {
@@ -208,19 +203,11 @@ router.get('/files', (req, res) => {
 // Route to execute the process of the selected file
 router.post('/execute', (req, res) => {
   const { fileName } = req.body;
-  const file = req.file;
-
-  const file = req.file;
-
-  if (!file) {
-    return res.status(400).json({ error: 'No file received' });
+  if (!fileName) {
+    return res.status(400).json({ error: 'No file selected' });
   }
-
   const filePath = path.join(__dirname, 'uploads', fileName);
   const command = `/usr/bin/python3 ${path.join(__dirname, 'process.py')} "${filePath}"`;
-  const filePath = file.path;
-  const targetDir = path.join(__dirname, 'uploads', path.parse(file.filename).name);
-
   exec(command, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error executing script: ${error}`);
@@ -229,31 +216,7 @@ router.post('/execute', (req, res) => {
     console.log(stdout);
     res.status(200).json({ message: 'File processed successfully' });
   });
-  fs.mkdirSync(targetDir, { recursive: true });
-
-  fs.createReadStream(filePath)
-    .pipe(unzipper.Extract({ path: targetDir }))
-    .on('close', () => {
-      const command = `/usr/bin/python3 ${path.join(__dirname, 'process.py')} "${targetDir}"`;
-	const time = {
- 	   timeout: 10 * 60 * 1000, };
-      exec(command, time, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error executing script: ${error}`);
-          return res.status(501).json({ error: 'Error processing file' });
-        }
-
-        console.log(stdout);
-
-        res.status(200).json({ message: 'File processed successfully' });
-      });
-    })
-    .on('error', (error) => {
-      console.error(`Error extracting ZIP file: ${error}`);
-      res.status(502).json({ error: 'Error processing file' });
-    });
 });
-
 
 module.exports = router;
 
